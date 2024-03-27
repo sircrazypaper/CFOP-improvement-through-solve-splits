9ce9e8c73fd369350293a8475d927a1c20162806
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2024-03-27 16:26:07 +0100

    updates in scripts

--- code.js
+++ code.js
@@ -4,11 +4,16 @@ let ollSpeed = 0;
 let pllSpeed = 0;
 let solveSpeed = 0;
 
+let f2lpct = 0.5735;
+let ollpct = 0.1721;
+let pllpct = 0.2544;
+
 
 function alerts(){
   f2lSpeed = document.getElementById('f2l-input').value;
   ollSpeed = document.getElementById('oll-input').value;
   pllSpeed = document.getElementById('pll-input').value;
   solveSpeed = parseFloat(f2lSpeed)+parseFloat(ollSpeed)+parseFloat(pllSpeed);
-  alert(solveSpeed);
+
+  document.getElementById('solve-speed').innerHTML = solveSpeed;
 }
