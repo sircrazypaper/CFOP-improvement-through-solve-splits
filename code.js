c3d121cb14047023b87e968df63287e6ee1de5b5
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2024-03-28 08:45:39 +0100

    Find out how much to improve f2l by

--- code.js
+++ code.js
@@ -8,12 +8,20 @@ let f2lpct = 0.5735;
 let ollpct = 0.1721;
 let pllpct = 0.2544;
 
+let f2lGoal = 0;
+let ollGoal = 0;
+let pllGoal = 0;
+
 
 function alerts(){
   f2lSpeed = document.getElementById('f2l-input').value;
   ollSpeed = document.getElementById('oll-input').value;
   pllSpeed = document.getElementById('pll-input').value;
   solveSpeed = parseFloat(f2lSpeed)+parseFloat(ollSpeed)+parseFloat(pllSpeed);
+  f2lGoal = solveSpeed*f2lpct;
+  ollGoal = solveSpeed*ollpct;
+  pllGoal = solveSpeed*pllpct;
 
-  document.getElementById('solve-speed').innerHTML = solveSpeed;
+  document.getElementById('improve-f2l-by').innerHTML = f2lGoal-f2lSpeed;
+
 }
