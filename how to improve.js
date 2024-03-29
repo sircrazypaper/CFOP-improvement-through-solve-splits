6e95f4508a35524d57cd7111baa9f43c9b80cde2
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2024-03-29 12:19:32 +0100

    Add comments and add improvement guides for >60s solvers

--- how to improve.js
+++ how to improve.js
@@ -1,4 +1,4 @@
-
+//Create all variables needed
 let f2lSpeed = 0;
 let ollSpeed = 0;
 let pllSpeed = 0;
@@ -16,10 +16,13 @@ let improveF2l = 0;
 let improveOll = 0;
 let improvePll = 0;
 
-let stepToImprove = " ";
+let stepToImprove = ' ';
+let improvementText = ' ';
 
 function improvementGuide(){
 
+
+  //Set all needed variables
   f2lSpeed = document.getElementById('f2l-input').value;
   ollSpeed = document.getElementById('oll-input').value;
   pllSpeed = document.getElementById('pll-input').value;
@@ -28,6 +31,7 @@ function improvementGuide(){
   ollGoal = solveSpeed*ollpct;
   pllGoal = solveSpeed*pllpct;
 
+  //Find out how much each step needs to improve
   improveF2l = f2lGoal-f2lSpeed;
   if(improveF2l>0){
     improveF2l = 0
@@ -41,6 +45,7 @@ function improvementGuide(){
     improvePll = 0
   }
 
+  //Find out step with most needed improvement
   if(improveF2l<improveOll && improveF2l<improvePll){
     stepToImprove = "F2L";
   } else if(improveOll<improveF2l && improveOll<improvePll){
@@ -51,8 +56,18 @@ function improvementGuide(){
     stepToImprove = "F2L";
   }
 
+  //Genererate all needed texts for improvement
+  if (solveSpeed>59.99 && stepToImprove=="F2L"){
+    improvementText = `<p> Your worst step is F2L. At your speed, you just have to do a ton of solves to improve your F2L speed. Don't forget to have fun with it!</p>`
+  } else if (solveSpeed>59.99 && stepToImprove=="OLL") {
+    improvementText = `<p> Your worst step is OLL. At your speed, you just have to do a ton of solves to improve your OLL speed. Don't forget to have fun with it!</p>`
+  } else if (solveSpeed>59.99 && stepToImprove=="PLL"){
+    improvementText = `<p> Your worst step is PLL. At your speed, you just have to do a ton of solves to improve your PLL speed. Don't forget to have fun with it!</p>`
+  }
+
+  //Display information
   document.getElementById('improve-f2l-by').innerHTML = f2lGoal.toFixed(2);
   document.getElementById('improve-oll-by').innerHTML = ollGoal.toFixed(2);
   document.getElementById('improve-pll-by').innerHTML = pllGoal.toFixed(2);
-  document.getElementById('step-to-improve').innerHTML = stepToImprove;
+  document.getElementById('step-to-improve').innerHTML = improvementText;
 }
